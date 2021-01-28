cf2233815d98bbef809fddf2db7c6fb94661a0ee
Hasitha N. Liyanage <hasithaliyan@gmail.com>
2021-01-28 17:20:50 +0530

    Create employees.js

---
+++ employees.js
@@ -0,0 +1,22 @@
+const employees = [
+  {
+    id: 1,
+    name: "john",
+    role: "manager",
+    dateOfBirth: "1980/10/04"
+  },
+  {
+    id: 2,
+    name: "sarah",
+    role: "executive",
+    dateOfBirth: "1980/05/14"
+  },
+  {
+    id: 3,
+    name: "will",
+    role: "accountant",
+    dateOfBirth: "1988/12/20"
+  }
+];
+
+module.exports = employees;
